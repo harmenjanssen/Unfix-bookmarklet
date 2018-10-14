8f105127ef86fc9ea3dc1b1f31af6214013d6596
Harmen Janssen <whatstyle@gmail.com>
2018-10-14 09:26:02 +0200

    Create unfix.js

---
+++ unfix.js
@@ -0,0 +1,25 @@
+// Click the bookmarklet, then click the fixed element.
+// Very quick n dirty, could use polishing probably.
+(function() {
+  'use strict';
+  const isFixed = elm => {
+    const cs = window.getComputedStyle(elm, null);
+    return cs ? cs.getPropertyValue('position') === 'fixed' : false;  
+  };
+  
+  const findInDomAncestry = (matchFn, node) => 
+    !node || node === document ? undefined :     
+      matchFn(node) ? node :
+        findInDomAncestry(matchFn, node.parentNode);
+    
+  const clickListener = (e) => {
+    const fixedTarget = findInDomAncestry(isFixed, e.target);
+    if (fixedTarget) {
+      fixedTarget.style.position = 'static';
+    }
+    document.body.style.cursor = '';
+    document.removeEventListener('click', clickListener);
+  };
+  document.body.style.cursor = 'crosshair';
+  document.addEventListener('click', clickListener);
+})();
